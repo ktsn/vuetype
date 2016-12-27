7a935dc977f9a53b6f68d40ee69094482153e7fd
ktsn <ktsn55@gmail.com>
2016-12-28 02:10:34 +0900

    Show help if there is no argument

--- src/bin/vuetype.ts
+++ src/bin/vuetype.ts
@@ -15,15 +15,19 @@ program
   .usage('<directory...>')
   .parse(process.argv)
 
-const patterns = program.args.map(arg => {
-  return path.join(arg, '**/*.vue')
-})
+if (program.args.length === 0) {
+  program.help()
+} else {
+  const patterns = program.args.map(arg => {
+    return path.join(arg, '**/*.vue')
+  })
 
-const root = detectDeepestRoot(program.args)
-findAndReadConfig(root).then(json => {
-  const options = json ? parseCompilerOptions(json, root) : {}
-  generate(globSync(patterns), options)
-})
+  const root = detectDeepestRoot(program.args)
+  findAndReadConfig(root).then(json => {
+    const options = json ? parseCompilerOptions(json, root) : {}
+    generate(globSync(patterns), options)
+  })
+}
 
 function detectDeepestRoot (pathNames: string[]): string {
   assert(pathNames.length >= 1)
