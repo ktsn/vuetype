6644f0db1f59423ffbbed300e397f4f1a0838f94
ktsn <ktsn55@gmail.com>
2017-02-12 10:33:59 +0900

    Split to findConfig and readConfig

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,14 +2,32 @@ import path = require('path')
 import ts = require('typescript')
 import { exists } from './file-util'
 
-export function findAndReadConfig (
-  baseDir: string,
-  _parseConfigHost: ts.ParseConfigHost = ts.sys, // for test
-  _exists: (filePath: string) => boolean = exists // for test
+export function readConfig (
+  configPath: string,
+  _parseConfigHost: ts.ParseConfigHost = ts.sys // for test
 ): ts.ParsedCommandLine | undefined {
+  const result = ts.readConfigFile(configPath, _parseConfigHost.readFile)
+
+  if (result.error) {
+    return undefined
+  }
+
+  return ts.parseJsonConfigFileContent(
+    result.config,
+    _parseConfigHost,
+    path.dirname(configPath),
+    undefined,
+    configPath
+  )
+}
+
+export function findConfig (
+  baseDir: string,
+  _exists: (filePath: string) => boolean = exists // for test
+): string | undefined {
   const configFileName = 'tsconfig.json'
 
-  function loop (dir: string): ts.ParsedCommandLine | undefined {
+  function loop (dir: string): string | undefined {
     const parentPath = path.dirname(dir)
     // It is root directory if parent and current dirname are the same
     if (dir === parentPath) {
@@ -17,19 +35,11 @@ export function findAndReadConfig (
     }
 
     const configPath = path.join(dir, configFileName)
-    if (!_exists(configPath)) {
-      return loop(parentPath)
+    if (_exists(configPath)) {
+      return configPath
     }
 
-    const result = ts.readConfigFile(configPath, _parseConfigHost.readFile)
-
-    return ts.parseJsonConfigFileContent(
-      result.config,
-      _parseConfigHost,
-      dir,
-      undefined,
-      configPath
-    )
+    return loop(parentPath)
   }
   return loop(baseDir)
 }
